cd27e2508f372c6c6aed48c3c8463e24d642a429
kasthurirangan2026 <kasthurirangan2026.git@gmail.com>
2025-11-18 20:00:57 +0530

    a

--- Assets/Scripts/CharacterControllerX.cs
+++ Assets/Scripts/CharacterControllerX.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class CharacterController : MonoBehaviour
+public class CharacterControllerX : MonoBehaviour
 {
     public LayerMask clickable;
 
